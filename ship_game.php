305e4e2c67041abcb3531e1fa380de513d441609
WebDev1119 <warriorjhs@outlook.com>
2020-01-23 22:17:35 +0800

    fix answer_100%

--- ship_game.php
+++ ship_game.php
@@ -1,79 +1,79 @@
-// you can write to stdout for debugging purposes, e.g.
-// print "this is a debug message\n";
+<?php
 
 function solution($N, $S, $T) {
-    // write your code in PHP7.0
+    $arr = initArray($N);
 
-    // init all positions
-    $positions = array();
-    for ($i = 0; $i < $N; $i++) {
-        $positions[$i] = array();
-        for ($j = 0; $j < $N; $j++) {
-            $positions[$i][$j] = -1;
-        }
-    }
-    
-    // get hit pos
-    $hit_str = explode(" ", $T);
-    $hit_pos = array();
-    for ($i = 0; $i < sizeof($hit_str); $i++) {
-        $hit_pos[$i] = array();
-        $row = (int)substr($hit_str[$i], 0, 1);
-        $col = ord(substr($hit_str[$i], 1, 1)) - 64;
-        $hit_pos[$i][0] = $row;
-        $hit_pos[$i][1] = $col;
-        
-        // set hit pos
-        $positions[$row - 1][$col - 1] = 0;
-    }
-    
-    // get ship pos
-    $ship_sunk = 0;
-    $ship_not_sunk = 0;
-    $ship_str = explode(",", $S);
-    $ship_pos = array();
-    for ($i = 0; $i < sizeof($ship_str); $i++) {
-        $ship_str1 = explode(" ", $ship_str[$i]);
-        $ship_pos[$i] = array();
-        for ($j = 0; $j < sizeof($ship_str1); $j++) {
-            $ship_pos[$i][$j] = array();
-            $row = (int)substr($ship_str1[$j], 0, 1);
-            $col = ord(substr($ship_str1[$j], 1, 1)) - 64;
-            $ship_pos[$i][$j][0] = $row;
-            $ship_pos[$i][$j][1] = $col;
-        }
-        
-        $status = getShipStatus($ship_pos[$i][0][0], $ship_pos[$i][0][1], $ship_pos[$i][1][0], $ship_pos[$i][1][1], $positions, $i + 1);
-        if ($status == 0) {
-            $ship_sunk++;
+    $arr = initHits($arr, $T);
+
+    $ships = explode(",", $S);
+
+    $sunk_count = 0; $hit_count = 0;
+    for ($i = 0; $i < sizeof($ships); $i++) {
+        $ship = explode(" ", $ships[$i]);
+
+        $x1 = intval($ship[0]) - 1;
+        $y1 = ord(str_replace($x1 + 1, "", $ship[0])) - 65;
+
+        $x2 = intval($ship[1]) - 1;
+        $y2 = ord(str_replace($x2 + 1, "", $ship[1])) - 65;
+
+        $status = getShipStatus($arr, $x1, $y1, $x2, $y2);
+
+        if ($status == -1) {
+            $sunk_count++;
         } else if ($status == 1) {
-            $ship_not_sunk++;
+            $hit_count++;
         }
     }
-    
-    return $ship_sunk . "," . $ship_not_sunk;
+
+    echo $sunk_count;
+    echo $hit_count;
 }
 
-function getShipStatus($s_row, $s_col, $e_row, $e_col, $arr, $index) {
-    $count_hit = 0;
-    $ship_cells = 0;
-    for ($i = $s_row - 1; $i < $e_row; $i++) {
-        for ($j = $s_col - 1; $j < $e_col; $j++) {
-            $ship_cells++;
-            if ($arr[$i][$j] < 0) {
-                $arr[$i][$j] = $index;
-            } else if ($arr[$i][$j] == 0) {
-                $count_hit++;
+function initArray($n) {
+    $arr = array();
+
+    for ($i = 0; $i < $n; $i++) {
+        $arr[$i] = array();
+        for ($j = 0; $j < $n; $j++) {
+            $arr[$i][$j] = -1;
+        }
+    }
+}
+
+function initHits($arr, $T) {
+    $hits = explode(" ", $T);
+    for ($i = 0; $i < sizeof($hits); $i++) {
+        $x = intval($hits[$i]) - 1;
+        $y = ord(str_replace($x + 1, "", $hits[$i])) - 65;
+
+        $arr[$x][$y] = 0;
+    }
+
+    return $arr;
+}
+
+function getShipStatus($arr, $x1, $y1, $x2, $y2) {
+    $hit_count = 0;
+
+    for ($i = $x1; $i <= $x2; $i++) {
+        for ($j = $y1; $j <= $y2; $j++) {
+            if ($arr[$i][$j] == 0) {
+                $hit_count++;
             }
         }
     }
-    if ($count_hit > 0) {
-        if ($count_hit == $ship_cells) {
-            return 0; // status sunk
+
+    $cell_count = ($x2 - $x1 + 1) * ($y2 - $y1 + 1);
+
+    if ($hit_count > 0) {
+        if ($hit_count == $cell_count) {
+            return -1; // Ship is sunk
         } else {
-            return 1; // hit but not sunk
+            return 1; // Hit but alive
         }
     } else {
-        return -1; // status ok
+        return 0; // Everything is ok
     }
 }
+?>
